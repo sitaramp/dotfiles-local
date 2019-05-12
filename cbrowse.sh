29bb9cc01aa7964734adca51e935194d738011e6
sitaramp <2187750+sitaramp@users.noreply.github.com>
2019-05-12 01:11:50 -0400

    add bash sensible, rearrange tmux.conf

--- cbrowse.sh
+++ cbrowse.sh
@@ -7,10 +7,10 @@ else
       return 1
 fi
 
-if [[ -e $PRODUCT_ROOT/cscope.out ]]; then
+if [[ -e $CALAVERAS_DIR/cscope.out ]]; then
      echo "Found cscope DB"
-     export CSCOPE_DB=$PRODUCT_ROOT
-     pushd $PRODUCT_ROOT
+     export CSCOPE_DB=$CALAVERAS_DIR
+     pushd $CALAVERAS_DIR
 else
       echo "failed to locate cscope DB. please set env using setupcal and symlinks"
       return 1
