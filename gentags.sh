ee0e5e979575e531f36772910ba3390408dbe681
sitaramp <2187750+sitaramp@users.noreply.github.com>
2019-05-14 08:11:56 -0400

    vim and tmux true color escape codes. gentags uses project env

--- gentags.sh
+++ gentags.sh
@@ -2,23 +2,24 @@
 
 # validate project root
 
-if ! [ -f "$PWD/DartCommon.mk" ];  then
-      echo "$0 failed: Wrong current directory. First cd to Dart directory"
+if ! [ -f "$CALAVERAS_DIR/DartCommon.mk" ];  then
+      echo "$0 failed: Failed to find Project directory Please setup projeuct dir"
       exit 1
 fi
+cd $CALAVERAS_DIR
 
 # Generate ctags
 ctags -R .
 
 # Generate cscope DB
-CSCOPE_DIR="$PWD/cscope"
+CSCOPE_DIR="$CALAVERAS_DIR/cscope"
 
 if [ ! -d "$CSCOPE_DIR" ]; then
 mkdir "$CSCOPE_DIR"
 fi
 
 echo "Finding files ..."
-find "$PWD" -name '*.[ch]' \
+find "$CALAVERAS_DIR" -name '*.[ch]' \
 -o -name '*.java' \
 -o -name '*properties' \
 -o -name '*.cpp' \
@@ -29,8 +30,8 @@ find "$PWD" -name '*.[ch]' \
 -o -name '*.py' \
 -o -name '*.php' > "$CSCOPE_DIR/cscope.files"
 
-echo "Adding files to cscope db: $PWD/cscope.db ..."
-cscope -b -i "$CSCOPE_DIR/cscope.files"
+echo "Adding files to cscope db: $CALAVERAS/cscope.db ..."
+cscope -b -u -C -i "$CSCOPE_DIR/cscope.files"
 
-export CSCOPE_DB="$PWD/cscope.out"
+export CSCOPE_DB="$CALAVERAS_DIR/cscope.out"
 echo "Exported CSCOPE_DB to: '$CSCOPE_DB'"
